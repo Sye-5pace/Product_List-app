8a63c3c70edec7afd39c5f0dffc24a40893bdc21
Silas Owu <owusilasyp00@gmail.com>
2024-08-21 07:38:09 -0700

    chore: setup project design system with tailwindcss for styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+
   theme: {
+    screens: {
+      'mobile': {'min':'200px','max':'427px'},
+      'tablet': {'min':'427px','max':'782px'},
+      'laptop': {'min':'782.1px'}
+    },
+    fontFamily:{
+      'redhat-text': ["Red Hat Text", "sans-serif"],
+    },
+    colors: {
+      'fantasy':'#FCF8F6',
+      'tiamaria':'#C73B0F',
+      'graphite':'#260F08',
+      'beaver':'#87635A',
+      'pharlap':'#AD8A85',
+      'clamshell':'#CAAFA7',
+      'dawnpink':'#F5EEEC',
+      'mountainmeadow':'#1EA575',
+    },
     extend: {},
   },
   plugins: [],
