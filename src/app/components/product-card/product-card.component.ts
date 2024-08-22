daab54785fae58e6a9979e5a5426865cc6461df0
Silas Owu <owusilasyp00@gmail.com>
2024-08-22 08:03:30 -0700

    refact: update active state for product card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -12,31 +12,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
-
 export class ProductCardComponent {
-
   product$!: Observable<IProduct[]>;
   error$!: Observable<any>;
   hoverIndex!: number | null;
   addTrigger: boolean[] = [];
-  cartAddState: boolean = false;
+  cartCounts: number[] = [];
 
   constructor(private productFacade: ProductsFacadeService,
-    private imageService: ResponsiveImagingService
-  ){
+              private imageService: ResponsiveImagingService) {
     this.product$ = this.productFacade.products$;
     this.error$ = this.productFacade.error$;
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.productFacade.loadProducts();
     this.product$.subscribe((products) => {
       this.addTrigger = new Array(products.length).fill(false);
+      this.cartCounts = new Array(products.length).fill(0); // Initialize cart counts for each product
     });
   }
 
-  getResponsiveImage(image: ImageType): string{
-    return this.imageService.getResponsiveImaging(image)
+  getResponsiveImage(image: ImageType): string {
+    return this.imageService.getResponsiveImaging(image);
   }
 
   onMouseEnter(index: number): void {
@@ -47,13 +45,28 @@ export class ProductCardComponent {
     this.hoverIndex = null;
   }
 
-  cartAddTrigger(i: any){
+  cartAddTrigger(i: any): void {
     this.addTrigger[i] = !this.addTrigger[i];
   }
+
   triggerHoverState(index: number): { [key: string]: boolean } {
     return {
       'border-tiamaria text-tiamaria': this.hoverIndex === index && !this.addTrigger[index],
       'border-pharlap text-graphite': this.hoverIndex !== index || this.addTrigger[index]
     };
+  }
+
+  addCart(i: number): void {
+    this.cartCounts[i]++;
+    this.addTrigger[i] = true;  
+  }
+
+  removeCart(i: number): void {
+    if (this.cartCounts[i] > 0) {
+      this.cartCounts[i]--;
+    }
+    if (this.cartCounts[i] === 0) {
+      this.addTrigger[i] = false;
+    }
   }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-import { ImageType, IProduct } from '../../store/model/product';
-import { Observable } from 'rxjs';
-import { ProductsFacadeService } from '../../services/products-facade.service';
 import { CommonModule } from '@angular/common';
-import { ResponsiveImagingService } from '../../services/responsive-imaging.service';
 import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
