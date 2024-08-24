74539fd3810d3f2ebf0f79b169bc9575bcf1086a
Silas Owu <owusilasyp00@gmail.com>
2024-08-24 09:32:29 -0700

    feat: persists store with ngrx-store-localstorage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,24 @@
+// app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideStore, StoreConfig } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
 import { productsReducer } from './store/products.reducer';
 import { ProductsEffects } from './store/products.effects';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+
+const localStorageSyncReducer = (reducer: any): any =>
+  localStorageSync({
+    keys: ['products'],
+    rehydrate: true
+  })
+(reducer);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideStore({ products: productsReducer}),
+    provideStore({ products: productsReducer }, { metaReducers: [localStorageSyncReducer] }),
     provideEffects([ProductsEffects]),
     provideHttpClient()
   ]
